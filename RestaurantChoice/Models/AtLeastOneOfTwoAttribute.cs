7e7020a70e284a4b593ad16536b65ae7cfd56709
Guillaume Beulaygue <guillaume.beulaygue@gmail.com>
2019-10-06 17:18:44 +0200

    Advanced validation

---
+++ RestaurantChoice/Models/AtLeastOneOfTwoAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantChoice.Models
+{
+    public class AtLeastOneOfTwoAttribute : ValidationAttribute, IClientValidatable
+    {
+        public string Parameter1 { get; set; }
+        public string Parameter2 { get; set; }
+
+        public AtLeastOneOfTwoAttribute() : base("You must enter at least one way to contact the restaurant")
+        {
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            PropertyInfo[] properties = validationContext.ObjectType.GetProperties();
+            PropertyInfo info1 = properties.FirstOrDefault(p => p.Name == Parameter1);
+            PropertyInfo info2 = properties.FirstOrDefault(p => p.Name == Parameter2);
+
+            string value1 = info1.GetValue(validationContext.ObjectInstance) as string;
+            string value2 = info2.GetValue(validationContext.ObjectInstance) as string;
+
+            if (string.IsNullOrWhiteSpace(value1) && string.IsNullOrWhiteSpace(value2))
+                return new ValidationResult(ErrorMessage);
+            return ValidationResult.Success;
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            ModelClientValidationRule rule = new ModelClientValidationRule();
+            rule.ValidationType = "verifcontact";
+            rule.ErrorMessage = ErrorMessage;
+            rule.ValidationParameters.Add("parameter1", Parameter1);
+            rule.ValidationParameters.Add("parameter2", Parameter2);
+
+            return new List<ModelClientValidationRule> { rule };
+        }
+    }
+}
--- RestaurantChoice/Models/Restaurant.cs
+++ RestaurantChoice/Models/Restaurant.cs
@@ -6,22 +6,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RestaurantChoice.Models
 {
-    public class Restaurant : IValidatableObject
+    public class Restaurant /*: IValidatableObject*/
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "The restaurant name must be filled"), StringLength(80)]
         public string Name { get; set; }
         //[Display(Name="Téléphone")]
-        [RegularExpression(@"^0[0-9]{9}$", ErrorMessage = "The phone number is incorrect")]
+        [RegularExpression(@"^0[0-9]{9}$", ErrorMessage = "The phone number is incorrect"), AtLeastOneOfTwoAttribute(Parameter1 = "PhoneNumber", Parameter2 = "Email", ErrorMessage = "You must enter at least one way to contact the restaurant")]
         public string PhoneNumber { get; set; }
+        [AtLeastOneOfTwoAttribute(Parameter1 = "PhoneNumber", Parameter2 = "Email", ErrorMessage = "You must enter at least one way to contact the restaurant")]
         public string Email { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (string.IsNullOrWhiteSpace(PhoneNumber)) && string.IsNullOrWhiteSpace(Email))
-            {
-                yield return new ValidationResult("ou must enter at least one way to contact the restaurant", new [] { "PhoneNumber", "Email" });
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (string.IsNullOrWhiteSpace(PhoneNumber) && string.IsNullOrWhiteSpace(Email))
+        //    {
+        //        yield return new ValidationResult("You must enter at least one way to contact the restaurant", new [] { "PhoneNumber", "Email" });
+        //    }
+        //}
     }
 }
